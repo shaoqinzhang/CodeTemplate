dec812dc074c380d71194f1cabe0b0e0486227b0
shaoqinzhang <1254264464@qq.com>
2022-01-21 21:28:25 +0800

    add android code for netbroadcast

--- Android/BroadCast/README.md
+++ Android/BroadCast/README.md
@@ -1,1 +1,4 @@
 # listen network environment
+
+intent action is android.net.conn.CONNECTIVITY_CHANGE
+you should have permission **android.permission.ACCESS_NETWORK_STATE**
---
+++ Android/BroadCast/netChangeReceive.java
@@ -0,0 +1,18 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class NetBroadCast extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//        uses-permission android:name="android.permission.ACCESS_NETWORK_STATE
+        ConnectivityManager systemService = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = systemService.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (networkInfo.isAvailable() && networkInfo.isConnected()) {
+            Toast.makeText(context,"network is enanle", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
